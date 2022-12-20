ea202bf1654aaae981e6b4619457dfb1cbab55c3
Mouaz El Molkey <moaz.mulki@outlook.com>
2022-12-20 12:31:42 +0300

    add recoil state mangment and updae readme

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,16 @@ import Layout from "components/layout/Layout/Layout";
 import CharactersCardSkeleton from "components/skeleton/CharactersCardSkeleton/CharactersCardSkeleton";
 import { GET_CHARACTERS } from "graphqModels/character/characters.query";
 import { ICharactersListResponse } from "models/characters/characters-types";
+import { useRecoilState } from "recoil";
+import { charactersAtom } from "recoil/atoms";
+
 
 function App() {
+  //Getting Rick and Morty Characters from the api
   const { loading, data } = useQuery<ICharactersListResponse>(GET_CHARACTERS);
+
+  //Init Recoil State (Only to show it)
+  const [characters, setCharacters] = useRecoilState(charactersAtom);
 
   return (
     <Layout>
--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,20 @@ import { theme } from "configs/defaulttheme";
 import { ApolloProvider } from "@apollo/client";
 import { graphInstance } from "api/ApolloClient";
 import { CssBaseline } from "@mui/material";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={graphInstance}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={graphInstance}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </ApolloProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
